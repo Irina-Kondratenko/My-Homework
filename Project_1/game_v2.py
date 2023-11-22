cbeea57f42f32a36e75ce53f25be3154fa87d061
Irina-Kondratenko <141589164+Irina-Kondratenko@users.noreply.github.com>
2023-11-23 00:16:21 +0300

    copy

--- Project_1/game_v2.py
+++ Project_1/game_v2.py
@@ -3,24 +3,35 @@
 """
 import numpy as np
 
-def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
-
+def random_predict(number: int = np.random.randint(1, 101)) -> int:
+    """Randomly guess a number
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
+        number (int, optional): Hidden number. Defaults to 1.
     Returns:
-        int: Число попыток
+        int: Number of attempts
     """
 
     count = 0
+    max_number = 100
+    min_number = 0
+    predict_number = np.random.randint(1, 101)  # prospective number
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
-        if number == predict_number:
-            break # выход из цикла, если угадали
-    return(count)
+
+        if predict_number > number:
+            max_number = predict_number - 1
+            predict_number = (max_number + min_number) // 2
+
+
+        elif predict_number < number:
+            min_number = predict_number + 1
+            predict_number = (max_number + min_number) // 2
+
+        else:
+            break  # end of the game, exit from the cycle
+
+    return count
 
 print(f'Количество попыток: {random_predict()}')
 
